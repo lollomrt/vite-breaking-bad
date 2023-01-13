106af2f90d75fa7e3ab987980b08a79420a89569
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-13 16:23:57 +0100

    Creazione area di lavoro

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+  
+<template lang="">
+  <div>
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
+  </div>
+  
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss'
+</style>
